98d62c7deec2834206e07ef7daf347e6f058465a
gejunxiang <gejunxiang@sogou-inc.com>
2018-10-30 17:40:25 +0800

    Commit Makefile

---
+++ Makefile
@@ -0,0 +1,18 @@
+obj=acmotion.so acmotion
+all: $(obj)
+.PHONE: all
+CC=g++
+CINCLUDES=-I /usr/local/include -I /usr/local/include/python3.5m/ -I /usr/include
+CLIBS=-L /usr/lib -lboost_python35
+CLIBS_2=$(CLIBS) -ldl -lutil -lpython3.5m
+FILES=wrapper.cpp TrieTree.h TrieTreeNode.h
+
+acmotion.so: $(FILES)
+	$(CC) -shared -o acmotion.so -fPIC $(FILES) $(CINCLUDES) $(CLIBS)
+
+acmotion: $(FILES)
+	$(CC) -g -o acmotion -FPIC $(FILES) $(CINCLUDES) $(CLIBS_2)
+
+.PHONE: clean
+clean:
+	-$(RM) $(obj)
--- make.sh
+++ make.sh
@@ -1,3 +1,4 @@
-g++ -shared -o acmotion.so -fPIC wrapper.cpp TrieTree.h TrieTreeNode.h -I /usr/local/include -I /usr/local/include/python3.6m/ -lunicode-encoding -lencoding -lboost_regex -lboost_python3 -lboost_date_time
-g++  -g -o acmotion wrapper.cpp TrieTree.h TrieTreeNode.h -I /usr/local/include -I /usr/local/include/python3.6m/ -lunicode-encoding -lencoding -lboost_regex -lboost_python3 -lboost_date_time -lpython3.6m
-
+#g++ -shared -o acmotion.so -fPIC wrapper.cpp TrieTree.h TrieTreeNode.h -I /usr/local/include -I /usr/local/include/python3.5m/ -lunicode-encoding -lencoding -lboost_regex -lboost_python35 -lboost_date_time
+g++ -shared -o acmotion.so -fPIC wrapper.cpp TrieTree.h TrieTreeNode.h -I /usr/local/include -I /usr/local/include/python3.5m/ -I /usr/include -L /usr/lib -lboost_python35
+#g++  -g -o acmotion wrapper.cpp TrieTree.h TrieTreeNode.h -I /usr/local/include -I /usr/local/include/python3.5m/ -lunicode-encoding -lencoding -lboost_regex -lboost_python35 -lboost_date_time -lpython3.5m
+g++  -g -o acmotion wrapper.cpp TrieTree.h TrieTreeNode.h -I /usr/local/include -I /usr/local/include/python3.5m/ -ldl -lutil -lboost_python35 -lpython3.5m
